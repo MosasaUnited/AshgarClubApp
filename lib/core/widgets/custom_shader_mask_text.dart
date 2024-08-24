6b2fa9fad150a44fd18cd39fbd53c47b86fac91e
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-08-24 11:28:11 +0300

    CreateCustomShaderMask

---
+++ lib/core/widgets/custom_shader_mask_text.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomShaderMaskText extends StatelessWidget {
+  const CustomShaderMaskText({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (bounds) {
+        return LinearGradient(
+          end: Alignment.centerRight,
+          begin: Alignment.centerLeft,
+          colors: [
+            Colors.purple.withAlpha(650),
+            Colors.cyanAccent.withAlpha(200),
+          ],
+        ).createShader(bounds);
+      },
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.titleMedium!.copyWith(
+              color: Colors.white,
+              fontSize: 28.sp,
+              fontWeight: FontWeight.w900,
+            ),
+      ),
+    );
+    const Placeholder();
+  }
+}
