d4c2e9e8c53aa7b21cfbddfab8976a9e53742907
Mostafa Mahmoud Saad <79766907+MosasaUnited@users.noreply.github.com>
2024-08-26 21:11:42 +0300

    BookedAppointmentsScreen

--- lib/features/sports/presentation/screens/booked_appointment.dart
+++ lib/features/sports/presentation/screens/booked_appointment.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shagra_club_app/core/widgets/custom_shader_mask_text.dart';
+
+import '../../../../core/theme/styles.dart';
 
 class BookedAppointment extends StatelessWidget {
   const BookedAppointment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CustomShaderMaskText(text: 'Your Schedule Booked'),
+          SizedBox(
+            height: 10.h,
+          ),
+          Card(
+            elevation: 0,
+            color: Colors.deepPurpleAccent,
+            shadowColor: Colors.yellow,
+            margin: EdgeInsets.zero,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            clipBehavior: Clip.antiAlias,
+            child: Text(
+              ' Courts 1 On Next Wednesday at 10:00 AM',
+              style: Styles.textStyle20.copyWith(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
